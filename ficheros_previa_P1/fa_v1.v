b84ffb081b16ed612a1c0a99a425d4c2b0e262e9
usuario <alu0101571669@ull.edu.es>
2023-10-04 11:33:48 +0000

    04-10-2023

--- ficheros_previa_P1/fa_v1.v
+++ ficheros_previa_P1/fa_v1.v
@@ -1,22 +1,7 @@
 //Sumador completo (full-adder) de tres entradas de 1 bit realizado a partir de puertas l�gicas 
 module fa(output wire sum, output wire c_out, input wire a, input wire b, input wire c_in);
 
-  assing sum = a ^ b;
-  assing c_out = a & b;
-
-endmodule
-
-module full_adder(output wire sum, output wire carry_out, input wire a, input wire b, input wire carry_in);
-
-  wire s1, c1, c2;
-
-  // Semisumador 1: a + b
-  semisum u1(s1, c1, a, b);
-
-  // Semisumador 2: s1 + carry_in
-  semisum u2(sum, c2, s1, carry_in);
-
-  // Carry Out del Full Adder
-  assign carry_out = c1 | c2; // OR de los acarreos generados por los semisumadores
+  assing sum = a ^ b ^ c_in; // Calcular la suma
+  assing c_out = (a & b) | (c_in & (a ^ b)); // Calcular el carry_out
 
 endmodule
