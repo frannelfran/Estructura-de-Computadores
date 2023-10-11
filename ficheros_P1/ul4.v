c7965bf81329d9e987c627146bceb0b28700761f
usuario <alu0101571669@ull.edu.es>
2023-10-11 14:09:06 +0000

    11-09-2023

--- ficheros_P1/ul4.v
+++ ficheros_P1/ul4.v
@@ -2,8 +2,8 @@
 // en el objetivo 2.1. El prototipo sería el siguiente:
 
 module ul4(output wire[3:0] Out, input wire[3:0] A, input wire[3:0] B, input wire [1:0] S);
-  cl c0(Out[0], A[0], B[0],S);
-  cl c1(Out[1], A[1], B[1],S);
-  cl c2(Out[2], A[2], B[2],S);
-  cl c3(Out[3], A[3], B[3],S);
+  cl cl0(Out[0], A[0], B[0],S);
+  cl cl1(Out[1], A[1], B[1],S);
+  cl cl2(Out[2], A[2], B[2],S);
+  cl cl3(Out[3], A[3], B[3],S);
 endmodule
