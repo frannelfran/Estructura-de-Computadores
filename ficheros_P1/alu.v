e58a9639c86a90bc13aa2959f08771273b8e0307
usuario <alu0101571669@ull.edu.es>
2023-10-21 18:00:10 +0000

    21/10/2023

--- ficheros_P1/alu.v
+++ ficheros_P1/alu.v
@@ -1,9 +1,4 @@
 module alu(output wire [3:0] R, output wire zero, c_out, sign, input wire [3:0] A, B, input wire c_in,
           input wire [1:0] ALUOP, input wire l);
 
-
-
-
-
-          
 endmodule
