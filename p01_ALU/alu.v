79f1e9eaa5089931a5aea9be71dd044cc3df2643
usuario <alu0101571669@ull.edu.es>
2023-10-24 15:33:01 +0000

    24/10/2023

--- p01_ALU/alu.v
+++ p01_ALU/alu.v
@@ -1,26 +1,29 @@
-module alu(output wire [3:0] R, output wire zero, c_out, sign, input wire [3:0] A, B, input wire c_in,
-          input wire [1:0] ALUOP, input wire l);
-  wire [3:0] sum_result;
-  wire sum_carry_out;
+module alu(output wire [3:0] R, output wire zero, c_out, sign, input wire [3:0] A, B, input wire c_in, input wire [1:0] ALUOP, input wire l);
+  input wire add1, opp1_A, opp2_B, cpl; // Entradas de los multiplexores
+  output wire [3:0] out_add1, out_opp1_A, out_opp2_B; // Salidas de los multiplexores
+  output wire [3:0] out_cpl; // Salidas de cpl
+  output wire [3:0] out_sum4, out_ul4; // Salidas para las operaciones aritméticas y lógicas
 
-  // Sumador de 4 bits
-  sum4 sum_unit(sum_result, sum_carry_out, A, B, ci_in);
-  // Realizar la operación según lo que marque la ALUOP
-  reg operacion;
-  always @(*) begin
-    case(ALUOP)
-      2'b00: operacion = sum_result; // Suma de A + B
-      2'b01: operacion = A & B; // A AND B
-      2'b10: operacion = A | B; // A OR B
-      2'b11: operacion = A ^ B; // A XOR B
-      default: operacion = 4'b0; // Valor por defecto
-    endcase
-  end
-  assign R = operacion;
-  // Detectar si la salida es igual a zero
-  assign zero = (R == 4'b0);
-  // Bit de acarreo de salida
-  assign c_out = sum_carry_out;
-  // Determinar el bit de signo
-  assign sign = R[3];
+  // Entrada de los multiplexores
+  assign add1 = ALUOP[0];
+  assign opp1_A = l | (ALUOP[1] & ~ALUOP[0]);
+  assign opp2_B = l | (ALUOP[1] & ~ALUOP[0]);
+  assign cpl = (l & ALUOP[1] & ALUOP[0]) | (~l & ~ALUOP[1] & ALUOP[0]);
+
+  // Creo los multiplexores de la ALU
+  mux2_4 mux_add1(out_add1, 4'b0000, 4'b0001, add1);
+  mux2_4 mux_op1_A(out_opp1_A, out_add1, A, opp1_A);
+  mux2_4 mux_op2_B(out_opp2_B, A, B, opp2_B);
+
+  //Creo el complemento a 1
+  compl1 compl1_cpl(out_cpl, out_opp2_B, cpl);
+
+  // Realizo las operaciones aritméticas
+  sum4 sum4_cin(out_sum4, c_out, out_opp1_A, out_opp2_B, c_in);
+
+  // Realizo las opoeraciones lógicas
+  ul4 ul4_aluop(out_ul4, out_opp1_A, out_cpl, ALUOP);
+
+  // Multiplexor para R
+  mux2_4 mux_R(R, out_sum4, out_ul4, l);
 endmodule
